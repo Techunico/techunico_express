61401bdf85f26a6c1a0ff06f1e82b661feb64b76
Praveen Kumar <praveen.42000@gmail.com>
2023-08-05 16:07:37 +0530

    minor changes

---
+++ core/createController.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Get the command-line arguments (parameters) passed to the script
+const args = process.argv.slice(2);
+
+// Display the parameters
+console.log('Command-line arguments:', args);
